f9a71944b8322aa81f9713d667c8bddba49165a3
Frederick Beaumont <frederickbeaumont32@gmail.com>
2024-06-13 12:04:00 -0400

    commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,13 +18,13 @@
 
         <div class="min-h-screen flex flex-row sm:justify-start items-center pt-6 sm:pt-0 bg-background bg-cover bg-center ">
 
-            
+
             <!-- <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>   -->
-            
+
             <div class="w-full sm:max-w-md ml-32 mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
